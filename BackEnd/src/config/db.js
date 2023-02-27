e3b6fbd5776b44265d36511ad2f440491a14095f
thanhnd183835 <ducthanh08061998@gmail.com>
2023-02-27 18:10:54 +0700

    fix mongose

--- BackEnd/src/config/db.js
+++ BackEnd/src/config/db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 
 const connectDB = async ()=>{
+    mongoose.set("strictQuery", true);
     const connection = await mongoose.connect(process.env.MONGO_URL, {
         useNewUrlParser:true,
         useUnifiedTopology: true,
